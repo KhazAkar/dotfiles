cc9fbc607340c439b1f0a93b7d22eb0d8185ee3f
KhazAkar <damianzrb@gmail.com>
2020-03-18 00:02:00 +0100

    fix autostart scripts, openbox config and add user contribution folder for rices

--- bin/scripts/apps/locker.sh
+++ bin/scripts/apps/locker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 exec xautolock -detectsleep \
-  -time 10 -locker "sh /home/khazakar/bin/scripts/apps/i3lock.sh" \
+  -time 10 -locker "sh $HOME/bin/scripts/apps/i3lock.sh" \
   -notify 30 \
   -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
--- bin/scripts/apps/tint2.sh
+++ bin/scripts/apps/tint2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CFGDIR="/home/khazakar/.config/tint2"
+CFGDIR="$HOME/.config/tint2"
 tint2 -c $CFGDIR/1stpanel.tint2rc 2> /dev/null &
 sleep 1s &
 tint2 -c $CFGDIR/2ndpanel.tint2rc 2> /dev/null &
